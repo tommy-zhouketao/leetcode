40cdba70664dd849aae7e8a54653f9be84e4b000
zhouketao <zhouketao@didichuxing.com>
2018-06-10 00:57:25 +0800

    add divide and conquer approach

--- Array/53.MaximumSubarray.cpp
+++ Array/53.MaximumSubarray.cpp
@@ -1,7 +1,48 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 using namespace std;
+
+class DivideAndConquer {
+public:
+    int sum;
+    int maxWithBegin;
+    int maxWithEnd;
+    int maximumSubArray;
+
+    DivideAndConquer(int sum, int maxWithBegin, int maxWithEnd, int maximumSubArray):
+        sum(sum), 
+        maxWithBegin(maxWithBegin), 
+        maxWithEnd(maxWithEnd), 
+        maximumSubArray(maximumSubArray)
+        {} 
+};
+
+class Solution2 {
+public:
+    DivideAndConquer divideAndConquer(vector<int>& nums) {
+        if (nums.size() == 1) {
+            return DivideAndConquer(nums[0], nums[0], nums[0], nums[0]);
+        }
+        vector<int> L(nums.begin(), nums.begin() + nums.size()/2);
+        vector<int> R(nums.begin() + nums.size()/2, nums.end());
+        DivideAndConquer l = divideAndConquer(L);
+        DivideAndConquer r = divideAndConquer(R);
+
+        return DivideAndConquer(
+            l.sum + r.sum,
+            max(l.maxWithBegin, l.sum + r.maxWithBegin),
+            max(r.maxWithEnd, r.sum + l.maxWithEnd),
+            max(max(l.maximumSubArray, r.maximumSubArray), l.maxWithEnd + r.maxWithBegin)
+            );
+    }
+
+    int maxSubArray(vector<int>& nums) {
+        DivideAndConquer dc = divideAndConquer(nums);
+        return dc.maximumSubArray;
+    }
+};
 
 class Solution {
 public:
